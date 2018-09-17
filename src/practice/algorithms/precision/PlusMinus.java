d7d15ded8451fc826dd134f21c08511bfa2cf3be
rDevelop <roblitvin@me.com>
2018-09-17 07:35:50 -0500

    Print precision of pos,neg,zeros in an array

---
+++ src/practice/algorithms/precision/PlusMinus.java
@@ -0,0 +1,50 @@
+package practice.algorithms.precision;
+
+import java.util.Scanner;
+
+public class PlusMinus {
+
+
+    // Complete the plusMinus function below.
+    static void plusMinus(int[] arr) {
+        int pos = 0;
+        int neg = 0;
+        int zero = 0;
+        for (int i : arr) {
+            if (i < 0) {
+                neg++;
+            } else if (i > 0) {
+                pos++;
+            } else {
+                zero++;
+            }
+        }
+        double val = (double) pos / arr.length;
+        System.out.printf("%.6f%n", ((double) pos / arr.length));
+        System.out.printf("%.6g%n", ((double) neg / arr.length));
+        System.out.printf("%.6g%n", ((double) zero / arr.length));
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] arr = new int[n];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        plusMinus(arr);
+
+        scanner.close();
+    }
+
+}
